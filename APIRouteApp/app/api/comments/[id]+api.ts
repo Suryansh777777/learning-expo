cef23f2903422a2f43e6372a2cf13e0790aabdeb
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-05-02 13:12:23 +0530

    Implement PATCH and DELETE endpoints for comments API

--- APIRouteApp/app/api/comments/[id]+api.ts
+++ APIRouteApp/app/api/comments/[id]+api.ts
@@ -7,3 +7,26 @@ export async function GET(_req: Request, { id }: Record<string, string>) {
   }
   return Response.json(comment);
 }
+
+export async function PATCH(request: Request, { id }: Record<string, string>) {
+  const { name, comment } = await request.json();
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === parseInt(id)
+  );
+  if (!commentIndex) {
+    return Response.json({ message: "Comment not found" }, { status: 404 });
+  }
+  comments[commentIndex] = { id: parseInt(id), name, comment };
+  return Response.json(comments[commentIndex]);
+}
+
+export async function DELETE(_req: Request, { id }: Record<string, string>) {
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === parseInt(id)
+  );
+  if (!commentIndex) {
+    return Response.json({ message: "Comment not found" }, { status: 404 });
+  }
+  comments.splice(commentIndex, 1);
+  return Response.json({ message: "Comment deleted" });
+}
